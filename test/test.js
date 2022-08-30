ee01e3aaa8e51b7befb7adb28858bf7b90f7908d
Sanjay Sunil <45319920+SanjaySunil@users.noreply.github.com>
2022-08-30 10:35:06 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -4,10 +4,9 @@ const ipv4 = require("../src/index");
 
 ipv4.static({
   interface: "eth0",
-  ip_address: "192.168.1.142",
+  ip_address: "192.168.1.146",
   subnet_mask: "255.255.255.0",
   gateway: "192.168.1.1",
   dns_server: "8.8.8.8",
   alternate_dns_server: "8.8.4.4",
 });
-  
